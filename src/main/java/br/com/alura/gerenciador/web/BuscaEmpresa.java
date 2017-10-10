5608dbe19f57ce3bf9e85a186dcf710862a3b7fd
Rafael Marques <rafaelmarques1992@gmail.com>
2017-10-10 10:34:52 -0300

    Implementação da aula 8

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -16,6 +16,22 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 @WebServlet(urlPatterns = "/busca")
 public class BuscaEmpresa extends HttpServlet {
 	
+	public BuscaEmpresa() {
+		System.out.println("Instanciando uma Servlet do tipo BuscaEmpresa " + this);
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Inicializando a Servlet " + this);
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("Destruindo a Servlet " + this);
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter writer = resp.getWriter();
